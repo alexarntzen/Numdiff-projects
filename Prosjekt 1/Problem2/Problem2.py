fb5a6fd95c5fa3608676e9d709173732dc80a65d
Alexander Arntzen <alexajar@stud.ntnu.no>
2020-02-11 23:55:31 +0100

    Fikset litt på koden så den er letter å lese

--- Prosjekt 1/Problem2/Problem2.py
+++ Prosjekt 1/Problem2/Problem2.py
@@ -12,87 +12,11 @@ from matplotlib import cm
 
 
 
-def get_Axy_square(bvp, N):
-    # Gridsize
-    h = 1.0 / n
 
-    # Total number of unknowns is N = (n+1)*(n+1)
-    N = (n + 1) * (n + 1)
-
-    # Define zero matrix A of right size and insert 0
-    A = sp.dok_matrix((N, N))
-
-    # Define FD entries of A
-    hh = h * h
-    for i in range(1, n):
-        for j in range(1, n):
-            A[I(i, j, n), I(i, j, n)] = 4 / hh  # U_ij
-            A[I(i, j, n), I(i - 1, j, n)] = -1 / hh  # U_{i-1,j}
-            A[I(i, j, n), I(i + 1, j, n)] = -1 / hh  # U_{i+1,j}
-            A[I(i, j, n), I(i, j - 1, n)] = -1 / hh  # U_{i,j-1}
-            A[I(i, j, n), I(i, j + 1, n)] = -1 / hh  # U_{i,j+1}
-
-
-
-    # N, Number of intervals
-    # Gridsize
-    h = 1 / N
-    # Total number of unknowns
-    Ni = N + 1
-    Ni2 = Ni * Ni
-    # Make the grid
-    x, y = np.ogrid[bvp.a:bvp.b:Ni * 1j, bvp.a:bvp.b:Ni * 1j]
-
-    # Define zero matrix A of right size and insert 0
-    A = sparse.dok_matrix((Ni2, Ni2))
-    # Define FD entries of A
-    muhh = - bvp.mu / (h * h)
-    h2 = 1 / (2 * h)
-
-    # get V
-    v1, v2 = bvp.V(x, y)
-    v1 = v1.ravel()
-    v2 = v2.ravel()
-    for i in range(1, N):
-        for j in range(1, N):
-            index = I(i, j, N)
-            A[index, index] = -4 * muhh  # U_ij, U_p
-            A[index, index - N - 1] = muhh - v2[i] * h2  # U_{i,j-1}, U_s
-            A[index, index + N + 1] = muhh + v2[i] * h2  # U_{i,j+1}, U_n
-            A[index, index - 1] = muhh - v1[j] * h2  # U_{i-1,j}, U_w
-            A[index, index + 1] = muhh + v1[j] * h2  # U_{i+1,j}, U_e
-
-        # Incorporate boundary conditions
-        # Add boundary values related to unknowns from the first and last grid ROW
-        for j in [0, N]:
-            for i in range(0, Ni):
-                index = I(i, j, N)
-                A[index, index] = 1
-
-        # Add boundary values related to unknowns from the first and last grid COLUMN
-        for i in [0, N]:
-            for j in range(0, Ni):
-                index = I(i, j, N)
-                A[index, index] = 1
-
-    return A, x, y
-
-
-
-
-class nonlinearPoisson():
-    def __init__(self, f):
-        self.f = f
-
-
-
-
-
-
+#Denne kan brukes når med Dirichlet conditions
 class finite_difference_BVP:
-
     @staticmethod
-    def getLinearizedBVP(scheme, g, numInternal, getInternalIndex, getCoordinate, getVecor,isBoundary, schemeCenter=1):
+    def getLinearizedBVPDirichlet(scheme, g, numInternal, getInternalIndex, getCoordinate, getVecor,isBoundary, schemeCenter=1):
         A = sparse.dok_matrix((numInternal, numInternal), dtype=np.float32)
         Right = np.zeros(numInternal)
         for internalIndex in range(numInternal):
@@ -107,7 +31,7 @@ class finite_difference_BVP:
         return A, Right
 
 
-
+#Denne kan også brukes i oppgave 1a
 class grid_BVP(finite_difference_BVP):
     def __init__(self,N,f,g,dim=2):
         self.N = N
@@ -137,33 +61,3 @@ class nonlinearPisson(grid_BVP):
     def constanScheme(self, vector):
         return np.array(([0, 1, 0], [1, -4, 1], [0, 1, 0])) * self.h ** 2
 
-
-
-
-
-
-
-
-    def solve():
-
-        N = 4
-        Ni = N-1
-
-        Ni2 = Ni**2
-        # Construct a sparse A-matrix
-        B = sparse.diags(Test.flatten(), [n for n in range(Test.size)], shape=(N + 1, N + 1), format="lil")
-
-        A = sparse.kron(sparse.eye(Ni), B)
-        C = sparse.diags([1, 1], [-Ni, Ni], shape=(Ni2, Ni2), format="lil")
-        A = (A + C).tocsr()  # Konverter til csr-format (nødvendig for spsolve)
-
-        C.toarray()
-
-        A = sparse.csr_matrix(np.array([[1, 0], [0,1]]))
-        sparse.kron(A, B).toarray()
-        B
-        A
-
-        x = np.linspace(0, 1, N+1)
-        y = np.linspace(0, 1, N+1)
-        h = 1/N
